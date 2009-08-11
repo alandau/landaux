9c6ba01c7b8d483b62904f1dded3db7a0dd85826
alex <alex@5a8e631d-7b07-0410-9aba-ee9232ada026>
2009-08-11 09:50:39 +0000

    exec_elf cleanups
    git-svn-id: file:///media/sf_Shared/landaux/repos/landaux/trunk@46 5a8e631d-7b07-0410-9aba-ee9232ada026

--- src/kernel/process.c
+++ src/kernel/process.c
@@ -99,76 +99,8 @@ void free_task(task_t *p)
 	free_page(p);
 }
 
-#define CODE_START	0x100000	/* 1MB */
 #define STACK_START	KERNEL_VIRT_ADDR
 #define STACK_SIZE	PAGE_SIZE
-
-#if 0
-int do_exec_flat(const char *path)
-{
-	int ret;
-	printk("do_exec_flat: path=<%s>\n", path);
-	file_t *f = vfs_open(path, O_RDONLY);
-	if (IS_ERR(f)) {
-		printk("do_exec: Can't open file '%s' (%d)\n", path, PTR_ERR(f));
-		return PTR_ERR(f);
-	}
-	u32 frame = alloc_phys_page();
-	void *p = (void *)CODE_START;
-	map_user_page(frame, (u32)p, 0 /* should be PTE_EXEC, but x86 sucks */);
-	ret = vfs_read(f, p, PAGE_SIZE);
-	if (ret < 0) {
-		printk("do_exec: Can't read from file '%s' (%d)\n", path, ret);
-		vfs_close(f);
-		goto out;
-	}
-	vfs_close(f);
-
-	/* setup stack */
-	u32 stack = alloc_phys_page();
-	p = (void *)(STACK_START - STACK_SIZE);
-	map_user_page(stack, (u32)p, PTE_WRITE);
-
-	/* setup mm areas */
-	ret = mm_add_area(CODE_START, PAGE_SIZE, VM_AREA_READ|VM_AREA_EXEC);
-	if (ret < 0)
-		goto out2;
-	ret = mm_add_area(STACK_START - STACK_SIZE, STACK_SIZE, VM_AREA_READ|VM_AREA_WRITE);
-	if (ret < 0)
-		goto out2;
-
-	regs_t *r = (regs_t *)((task_stack_t *)current + 1) - 1;
-	r->eax = r->ebx = r->ecx = r->edx = r->esi = r->edi = r->ebp = 0;
-	r->cs = USER_CS;
-	r->ds = r->es = r->fs = r->gs = r->ss = USER_DS;
-	r->eip = CODE_START;
-	r->esp = STACK_START;
-	r->eflags = (1<<9) | (1<<1);	// 9 is IF, 1 is always on
-	printk("do_exec: success\n");
-	return 0;
-out2:
-	free_phys_page(stack);
-out:
-	free_phys_page(frame);
-	return ret;
-}
-#endif
-
-static u32 elf_flags_to_pte_flags(Elf32_Word flags)
-{
-	return flags & PF_W ? PTE_WRITE : 0;
-}
-
-#if 0
-static u32 elf_flags_to_area_flags(Elf32_Word flags)
-{
-	u32 ret = 0;
-	if (flags & PF_R) ret |= VM_AREA_READ;
-	if (flags & PF_W) ret |= VM_AREA_WRITE;
-	if (flags & PF_X) ret |= VM_AREA_EXEC;
-	return ret;
-}
-#endif
 
 static u32 elf_flags_to_mmap_prot(Elf32_Word flags)
 {
@@ -179,7 +111,7 @@ static u32 elf_flags_to_mmap_prot(Elf32_Word flags)
 	return ret;
 }
 
-int do_exec_elf(const char *path)
+static int do_exec_elf(const char *path)
 {
 	int ret;
 	printk("do_exec_elf: path=<%s>\n", path);
@@ -259,9 +191,10 @@ int do_exec_elf(const char *path)
 			u32 virt_addr = phdr.p_vaddr - offset_in_page;
 			u32 read_size = phdr.p_filesz + offset_in_page;
 			u32 mem_size = phdr.p_memsz + offset_in_page;
-#if 0
-			u32 flags = elf_flags_to_pte_flags(phdr.p_flags);
-#endif
+			if (virt_addr > KERNEL_VIRT_ADDR) {
+				printk("do_exec_elf: Bad virt addr 0x%x in segment %d '%s' (%d)\n", virt_addr, i, path);
+				return -ENOEXEC;
+			}
 			u32 prot = elf_flags_to_mmap_prot(phdr.p_flags);
 			ret = PTR_ERR(do_mmap(f, file_offset, virt_addr, read_size, prot, MAP_PRIVATE|MAP_FIXED));
 			if (IS_ERR(ERR_PTR(ret))) {
@@ -275,40 +208,6 @@ int do_exec_elf(const char *path)
 					return ret;
 				}
 			}
-#if 0
-			int j;
-			for (j = 0; j < mem_size >> PAGE_SHIFT; j++) {
-				u32 frame = alloc_phys_page();
-				map_user_page(frame, virt_addr + (j<<PAGE_SHIFT), flags);
-			}
-			ret = vfs_lseek(f, file_offset, SEEK_SET);
-			if (ret < 0) {
-				vfs_close(f);
-				printk("do_exec_elf: Can't seek in segment past EOF '%s' (%d)\n", path, ret);
-				return ret;
-			}
-			u32 count = 0;
-			while (count < read_size) {
-				ret = vfs_read(f, (void *)(virt_addr + count), read_size - count);
-				if (ret < 0) {
-					vfs_close(f);
-					printk("do_exec_elf: Can't read segment data %d '%s' (%d)\n", i, path, ret);
-					return ret;
-				}
-				if (ret == 0) {
-					vfs_close(f);
-					printk("do_exec_elf: Reached EOF in segment %d '%s'\n", i, path);
-					return -ENOEXEC;
-				}
-				count += ret;
-			}
-			memset((void *)(virt_addr + read_size), 0, mem_size - read_size);
-			ret = mm_add_area(virt_addr, mem_size, elf_flags_to_area_flags(phdr.p_flags));
-			if (ret < 0) {
-				printk("do_exec_elf: mm_add_area failed in segment %d '%s' (%d)\n", i, path, ret);
-				return ret;
-			}
-#endif
 			break;
 		case PT_GNU_STACK:
 			stack_flags = phdr.p_flags;
@@ -323,11 +222,6 @@ int do_exec_elf(const char *path)
 	vfs_close(f);
 
 	/* setup stack */
-#if 0
-	u32 stack = alloc_phys_page();
-	map_user_page(stack, STACK_START - STACK_SIZE, elf_flags_to_pte_flags(stack_flags));
-	ret = mm_add_area(STACK_START - STACK_SIZE, STACK_SIZE, elf_flags_to_area_flags(stack_flags));
-#endif
 	ret = PTR_ERR(do_mmap(NULL, 0, STACK_START - STACK_SIZE, STACK_SIZE, elf_flags_to_mmap_prot(stack_flags), MAP_PRIVATE|MAP_FIXED));
 	if (IS_ERR(ERR_PTR(ret))) {
 		printk("do_exec_elf: do_mmap failed for stack '%s' (%d)\n", path, ret);
@@ -347,33 +241,6 @@ int do_exec_elf(const char *path)
 
 int sys_exec(const char *path)
 {
-#if 0
-#define CODE_START (1024*1024)	// 1MB
-#define STACK_START KERNEL_VIRT_ADDR
-	u32 code_size = end_process_code - process_code;
-	u32 code_pages = (code_size + PAGE_SIZE-1) >> PAGE_SHIFT;
-	int i;
-	// map code
-	for (i=0; i<code_pages; i++)
-	{
-		u32 frame = alloc_phys_page();
-		if (frame == 0) return -1;
-		if (ioremap(frame << PAGE_SHIFT, PAGE_SIZE, CODE_START+(i<<PAGE_SHIFT), MAP_USERSPACE) == NULL)
-			return -1;
-	}
-	// map stack
-	u32 frame = alloc_phys_page();
-	if (frame == 0) return -1;
-	if (ioremap(frame << PAGE_SHIFT, PAGE_SIZE, STACK_START-(1<<PAGE_SHIFT), MAP_USERSPACE | MAP_READWRITE) == NULL)
-		return -1;
-
-	// copy code and setup usermode registers
-	memcpy((void *)CODE_START, process_code, code_size);
-	regs_t *regs = (regs_t *)((task_stack_t *)current + 1) - 1;
-	regs->eflags = (1<<9) | (1<<1);	// 9 is IF, 1 is always on
-	regs->eip = CODE_START;
-	regs->esp = STACK_START;
-#endif
 	return do_exec_elf(path);
 }
 
